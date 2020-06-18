6b6a10dac7f6cda8f33322c2d23e6ed277d689e1
heyfgirl <heyfgirl@163.com>
2020-06-18 14:16:48 +0800

    接口名称更改

--- app/routes/index.js
+++ app/routes/index.js
@@ -8,7 +8,7 @@ module.exports = function(app) {
     // 获取房间列表
     app.route('/get/rooms').all(AuthUser(), UserController.GetRooms);
     app.route('/get/room').all(AuthUser(), UserController.GetRoom);
-    app.route('/get/uset_token').all(UserController.Register);// 注册用户获取用户连接token
-    app.route('/get/message').all(AuthUser(), UserController.GetMessages);// 注册用户获取用户连接token
+    app.route('/get/user_token').all(UserController.Register);// 注册用户获取用户连接token
+    app.route('/get/messages').all(AuthUser(), UserController.GetMessages);// 注册用户获取用户连接token
 };
 
